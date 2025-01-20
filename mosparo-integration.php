55dc236e54a9eeb1423c557a2d4f0a0973b3211a
Matthias Zobrist <matthias.zobrist@zepi.net>
2025-01-20 19:57:46 +0100

    Bumped the version number to v1.12.2

--- mosparo-integration.php
+++ mosparo-integration.php
@@ -14,7 +14,7 @@
  * Author:            mosparo
  * Author URI:        https://mosparo.io/
  * License:           MIT
- * Version:           1.12.1
+ * Version:           1.12.2
  * Text Domain:       mosparo-integration
  * Domain Path:       /languages
  */
